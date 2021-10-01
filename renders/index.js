3dfa5f5844290ce420b73a69de298efed191d442
LAPTOP-SMOR46KU\strav <stravinest@naver.com>
2021-10-01 22:31:44 +0900

    초기설정

---
+++ renders/index.js
@@ -0,0 +1,15 @@
+ const express = require("express");
+
+// const viewRouter = require("./view");
+// const modifyRouter = require("./modify");
+// const mainRouter = require('./main');
+// const writeRouter = require('./write.js');
+
+ const router = express.Router();
+
+// router.use("/", mainRouter); // ./main 실행 
+// router.use("/view", viewRouter);
+// router.use("/write", writeRouter);
+// router.use("/modify", modifyRouter);
+
+ module.exports = router;
---
+++ routers/index.js
@@ -0,0 +1,8 @@
+const express = require("express");
+//const Posts = require("./router_Posts");
+
+const router = express.Router();
+
+//router.use('/posts/', Posts);//주소창에 /api/posts 로 나오면 router_posts.js파일 실행 하러간다.!!
+
+module.exports = router;
