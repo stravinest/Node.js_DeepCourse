f8948d49f6d9c725707fdcdec9516540a332d014
LAPTOP-SMOR46KU\strav <stravinest@naver.com>
2021-10-01 22:56:20 +0900

    초기설정2

--- renders/index.js
+++ renders/index.js
@@ -1,15 +1,15 @@
- const express = require("express");
+const express = require('express');
 
+const mainRouter = require('./main');
 // const viewRouter = require("./view");
 // const modifyRouter = require("./modify");
-// const mainRouter = require('./main');
 // const writeRouter = require('./write.js');
 
- const router = express.Router();
+const router = express.Router();
 
-// router.use("/", mainRouter); // ./main 실행 
+router.use('/', mainRouter); // ./main 실행
 // router.use("/view", viewRouter);
 // router.use("/write", writeRouter);
 // router.use("/modify", modifyRouter);
 
- module.exports = router;
+module.exports = router;
